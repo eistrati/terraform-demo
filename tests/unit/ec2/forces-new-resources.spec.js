9e5d55343435a6ba9feb5d28385e709f98f2981f
Eugene <eistrati@mitocgroup.com>
2018-01-11 14:10:34 -0500

    fix unit test

--- tests/unit/ec2/forces-new-resources.spec.js
+++ tests/unit/ec2/forces-new-resources.spec.js
@@ -3,13 +3,13 @@ let fs = require('fs');
 let path = require('path');
 
 describe('Terraform show log assertion', () => {
-  it('Test terraform show log should fail if contains "forces-new-resources" keyword', () => {
+  it('Test terraform show log should fail if contains "new resource required" keyword', () => {
     let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/.resource/terraform-show.txt'));
-    chai.expect(data.toString(),  '"forces-new-resources" keyword is detected in terraform show log').not.to.have.string('forces-new-resources')
+    chai.expect(data.toString(),  '"new resource required" keyword is detected in terraform show log').not.to.have.string('new resource required')
   });
 
   it('Test terraform show log should fail if contains "forces new resources" keyword', () => {
     let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/.resource/terraform-show.txt'));
-    chai.expect(data.toString(),  '"forces new resources" keyword is detected in terraform show log').not.to.have.string('forces-new-resources')
+    chai.expect(data.toString(),  '"forces new resources" keyword is detected in terraform show log').not.to.have.string('forces new resources')
   });
 });
