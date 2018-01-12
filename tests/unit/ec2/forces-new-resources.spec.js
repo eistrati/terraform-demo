ff6b0a944bbaaa264d712e201972751d4505172e
avozicov <avozicov@pycoding.biz>
2018-01-12 12:51:47 +0200

    debug

--- tests/unit/ec2/forces-new-resources.spec.js
+++ tests/unit/ec2/forces-new-resources.spec.js
@@ -4,12 +4,12 @@ let path = require('path');
 
 describe('Terraform show log assertion', () => {
   it('Test terraform show log should fail if contains "new resource required" keyword', () => {
-    let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/tfshow.txt'));
+    let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/.resource/tfshow.txt'));
     chai.expect(data.toString(),  '"new resource required" keyword is detected in terraform show log').not.to.have.string('new resource required')
   });
 
   it('Test terraform show log should fail if contains "forces new resources" keyword', () => {
-    let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/tfshow.txt'));
+    let data = fs.readFileSync(path.resolve(__dirname, '../../../ec2/.resource/tfshow.txt'));
     chai.expect(data.toString(),  '"forces new resources" keyword is detected in terraform show log').not.to.have.string('forces new resource')
   });
 });
