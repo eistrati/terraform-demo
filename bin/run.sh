6565eea4cf0c42bfdcff27edc696942527133c53
Eugene <eistrati@mitocgroup.com>
2018-01-11 23:14:23 -0500

    minor refactoring

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-APP_PATH=$(dirname $(cd $(dirname $0); pwd -P))
-SCREENSHOTS_DIR="${APP_PATH}/tests/e2e/screenshots/"
-S3_PATH="s3://travis-metadata/cache/MitocGroup/terraform-demo/screenshots/${JOB_NAME}/${BUILD_NUMBER}/"
 
-on_fail() {
+function on_fail() {
     if [ -d "${SCREENSHOTS_DIR}" ]; then
         echo "Sync screenshots directory to S3 ..."
         aws s3 sync ${SCREENSHOTS_DIR} ${S3_PATH} --acl 'public-read'
@@ -13,11 +10,15 @@ on_fail() {
 }
 
 echo "Running 'recink run terraform' ..."
-recink run terraform --include-modules="ec2" --custom-config="ec2.terraform.plan:true,ec2.terraform.apply:false,ec2.terraform.save-show-output:terraform-show.txt" || on_fail
+recink run terraform -v --include-modules="ec2" --custom-config="ec2.terraform.plan:true,ec2.terraform.apply:false,ec2.terraform.save-show-output:tfshow.txt" || on_fail
 recink run unit || on_fail
-recink run terraform --include-modules="ec2" --custom-config="ec2.terraform.plan:false,ec2.terraform.apply:true" || on_fail
+recink run terraform -v --include-modules="ec2" --custom-config="ec2.terraform.plan:false,ec2.terraform.apply:true" || on_fail
 
-TF_STATE=${APP_PATH}/ec2/.resource/terraform.tfstate
+APP_PATH=$(dirname $(cd $(dirname $0); pwd -P))
+SCREENSHOTS_DIR="${APP_PATH}/tests/e2e/screenshots/"
+S3_PATH="s3://travis-metadata/cache/MitocGroup/terraform-demo/screenshots/${JOB_NAME}/${BUILD_NUMBER}/"
+TF_STATE="${APP_PATH}/ec2/.resource/terraform.tfstate"
+
 if [ -f ${TF_STATE} ]; then
    echo "Copying .tfstate files to tests directory"
    mkdir -p ${APP_PATH}/tests/e2e/ec2/.resource/
