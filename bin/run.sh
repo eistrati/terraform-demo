eb0547de83cdc4629f57674b1350d9d399475d8b
Eugene <eistrati@mitocgroup.com>
2018-01-15 08:00:05 -0500

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+APP_PATH=$(dirname $(cd $(dirname $0); pwd -P))
+SCREENSHOTS_DIR="${APP_PATH}/tests/e2e/screenshots/"
+S3_PATH="s3://travis-metadata/cache/MitocGroup/terraform-demo/screenshots/${JOB_NAME}/${BUILD_NUMBER}/"
+
 function on_fail() {
     if [ -d "${SCREENSHOTS_DIR}" ]; then
         echo "Sync screenshots directory to S3 ..."
@@ -14,15 +18,9 @@ recink run terraform --include-modules="ec2" --custom-config="ec2.terraform.plan
 recink run unit || on_fail
 recink run terraform --include-modules="ec2" --custom-config="ec2.terraform.plan:true,ec2.terraform.apply:true" || on_fail
 
-APP_PATH=$(dirname $(cd $(dirname $0); pwd -P))
-SCREENSHOTS_DIR="${APP_PATH}/tests/e2e/screenshots/"
-S3_PATH="s3://travis-metadata/cache/MitocGroup/terraform-demo/screenshots/${JOB_NAME}/${BUILD_NUMBER}/"
-TF_STATE="${APP_PATH}/ec2/.resource/terraform.tfstate"
+ls -al ${APP_PATH}/ec2/.resource/
 
-if [ -f ${TF_STATE} ]; then
-   echo "Copying .tfstate files to tests directory"
-   mkdir -p ${APP_PATH}/tests/e2e/ec2/.resource/
-   cp ${TF_STATE} ${APP_PATH}/tests/e2e/ec2/.resource/terraform.tfstate.json
+if [ -f "${APP_PATH}/ec2/.resource/terraform.tfstate" ]; then
    echo "Running 'recink run e2e' ..."
    recink run e2e || on_fail
 else
