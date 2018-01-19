f3995f7c07c28828700a15508343c0700937b7b3
avozicov <avozicov@pycoding.biz>
2018-01-19 16:11:00 +0200

    debug Jenkins execution with test data

---
+++ tests/e2e/config/test-data.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    "type": 'aws_vpc',
+    "region": 'us-west-2',
+    "id": 'vpc-a0c91fc5',
+    "status": 'available'
+  },
+  {
+    "type": 'aws_subnet',
+    "region": 'us-west-2',
+    "id": 'subnet-4e3afa39',
+    "status": 'available'
+  },
+  {
+    "type": 'aws_route_table',
+    "region": 'us-west-2',
+    "id": 'rtb-098e5e6c'
+  },
+  {
+    "type": 'aws_internet_gateway',
+    "region": 'us-west-2',
+    "id": 'igw-b9d32bdc',
+    "status": 'attached'
+  },
+  {
+    "type": 'aws_default_vpc_dhcp_options',
+    "region": 'us-west-2',
+    "id": 'dopt-685e450a'
+  },
+  {
+    "type": 'aws_network_acl',
+    "region": 'us-west-2',
+    "id": 'acl-feca1d9b'
+  },
+  {
+    "type": 'aws_security_group',
+    "region": 'us-west-2',
+    "id": 'sg-25442640'
+  },
+  {
+    "type": 'aws_instance',
+    "region": 'us-west-2',
+    "id": 'i-034d837ea393576d4',
+    "status": 'running'
+  },
+  {
+    "type": 'aws_db_instance',
+    "region": 'us-west-2',
+    "id": 'wos-caesar-new',
+    "status": 'available'
+  }
+]
