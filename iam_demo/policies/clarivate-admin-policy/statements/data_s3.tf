5858f2b3fa472981fb120a20d6f665d7730c5a38
Eugene <eistrati@mitocgroup.com>
2018-02-16 11:44:44 -0500

    fix principals issue

--- iam_demo/policies/clarivate-admin-policy/statements/data_s3.tf
+++ iam_demo/policies/clarivate-admin-policy/statements/data_s3.tf
@@ -1,10 +1,5 @@
 data "aws_iam_policy_document" "statement_s3" {
   statement {
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
     actions   = ["s3:*"]
     resources = ["*"]
   }
--- iam_demo/policies/clarivate-admin-policy/statements/statement.tf
+++ iam_demo/policies/clarivate-admin-policy/statements/statement.tf
@@ -2,11 +2,6 @@ data "aws_iam_policy_document" "statement" {
   override_json = "${data.aws_iam_policy_document.statement_s3.json}"
 
   statement {
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
 	actions   = ["ec2:*"]
 	resources = ["*"]
   }
