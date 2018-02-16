1825a306d82c4d235a59a425ea926b9a3576e6cf
Eugene <eistrati@mitocgroup.com>
2018-02-16 11:42:15 -0500

    update principals

--- iam_demo/policies/clarivate-admin-policy/statements/data_s3.tf
+++ iam_demo/policies/clarivate-admin-policy/statements/data_s3.tf
@@ -1,5 +1,10 @@
 data "aws_iam_policy_document" "statement_s3" {
   statement {
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
     actions   = ["s3:*"]
     resources = ["*"]
   }
--- iam_demo/policies/clarivate-admin-policy/statements/statement.tf
+++ iam_demo/policies/clarivate-admin-policy/statements/statement.tf
@@ -2,6 +2,11 @@ data "aws_iam_policy_document" "statement" {
   override_json = "${data.aws_iam_policy_document.statement_s3.json}"
 
   statement {
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
 	actions   = ["ec2:*"]
 	resources = ["*"]
   }
