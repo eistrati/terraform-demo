da645fc031f4e9ee00244e9491a14792e764f31d
Eugene <eistrati@mitocgroup.com>
2018-02-27 16:50:18 -0500

    remove dynamodb

--- iam/policies/admin-policy/statements/statement_01.tf
+++ iam/policies/admin-policy/statements/statement_01.tf
@@ -1,5 +1,5 @@
 data "aws_iam_policy_document" "statement_01" {
-  override_json = "${data.aws_iam_policy_document.statement_02.json}"
+  # override_json = "${data.aws_iam_policy_document.statement_02.json}"
 
   statement {
     actions   = ["s3:*"]
